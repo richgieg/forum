b6fb49475b52292f27c20ca36db773f2ba9e1d08
Richard Gieg <richgieg@users.noreply.github.com>
2015-09-16 11:20:22 -0700

    Remove semicolons from query strings

--- forumdb.py
+++ forumdb.py
@@ -15,7 +15,7 @@ def GetAllPosts():
     '''
     DB = psycopg2.connect("dbname=forum")
     c = DB.cursor()
-    c.execute("SELECT time, content FROM posts ORDER BY time DESC;")
+    c.execute("SELECT time, content FROM posts ORDER BY time DESC")
     posts = [{'content': str(row[1]), 'time': str(row[0])}
              for row in c.fetchall()]
     DB.close()
@@ -30,6 +30,6 @@ def AddPost(content):
     '''
     DB = psycopg2.connect("dbname=forum")
     c = DB.cursor()
-    c.execute("INSERT INTO posts (content) VALUES (%s);", (content,))
+    c.execute("INSERT INTO posts (content) VALUES (%s)", (content,))
     DB.commit()
     DB.close()
